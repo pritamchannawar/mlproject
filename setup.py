f075b07f7ee9397c0311b63a7d2ef5a2ecd00894
pritamchannawar <channawar.pritam@gmail.com>
2024-04-05 18:50:42 -0400

    updates

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='ML_end-to-end-Project',
+name='mlproject',
 version='0.0.1',
 author='pritamchannawar',
 author_email='channawar.pritam@gmail.com',
