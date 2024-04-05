619a5f76aefd874cb223f69e9f48fbc80a0bae11
pritamchannawar <channawar.pritam@gmail.com>
2024-04-05 19:21:32 -0400

    updates

--- src/exception.py
+++ src/exception.py
@@ -20,5 +20,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-
-        
+if __name__== "__main__":
+    try:
+        a = 1/0
+    except:
+        raise CustomException
+        logging.info("devide by 0 error")        
